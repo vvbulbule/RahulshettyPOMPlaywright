96eb61c2c13802ded2b5d3b1a8e17ed2ad139aa5
vikrantpathlock <vikrant.bulbule@pathlock.com>
2026-07-07 17:41:11 +0530

    Cross Browser Testing

---
+++ playwright2.config.ts
@@ -0,0 +1,47 @@
+import { chromium, defineConfig, devices } from '@playwright/test';
+
+
+// All Configuration key value pair is stored in this
+// we need to export this defineConfig so that it will available to all files in the project
+export default defineConfig({
+  //const  config=({
+  testDir: './tests',
+  //by default timeout is of 30 sec 
+  // only if we want to overwrite the default timeout we need to defined it otherwise no need
+  // Timeout of 40 sec to wait for perticular element
+  // this time out is for every steps in playwright and components
+  timeout:40 *1000,
+  // for assertion validation we use expect timeout
+  expect:{
+  timeout: 5000,
+  },
+ // Once all the test run below line give the report in html
+ reporter:'html',
+
+ projects : [
+
+    {
+        name : "Firefox",
+        use: {
+    browserName:'firefox',
+    headless: true,
+    screenshot :'on',
+    trace : 'retain-on-failure'//off,on,retain-on-failure
+            }
+    },
+
+    {
+        name : "Chrome",
+        use: {
+    browserName:'chromium',
+    headless: false,
+    screenshot :'on',
+    trace : 'retain-on-failure'//off,on,retain-on-failure
+            }
+    }
+    
+ ]
+  
+  ,
+
+});
